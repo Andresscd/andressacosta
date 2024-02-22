42a4007fac1e5c39ef4d56ff9f19b16fe64e9ecf
andressa <160681599+Andresscd@users.noreply.github.com>
2024-02-22 20:55:41 +0100

    arquivos do meu portfolio

---
+++ contact.js
@@ -0,0 +1,25 @@
+document.getElementById('form-contato').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita o envio padrão do formulário
+
+    // Recupera os dados do formulário
+    var nome = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var mensagem = document.getElementById('nachricht').value;
+
+    // Constrói o objeto de dados para enviar
+    var dados = {
+        name: nome,
+        email: email,
+        mensagem: nachricht
+    };
+
+    // Envia os dados para o serviço de e-mail usando EmailJS
+    emailjs.send('service_andressa', 'template_6kj6ldm', dados)
+    .then(function(response) {
+        console.log('E-mail enviado com sucesso:', response);
+        alert('E-mail enviado com sucesso!');
+    }, function(error) {
+        console.error('Ocorreu um erro ao enviar o e-mail:', error);
+        alert('Ocorreu um erro ao enviar o e-mail.');
+    });
+});
